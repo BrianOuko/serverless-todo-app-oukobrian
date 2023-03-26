4630ee258d9350773b2c2dc23ea3ffba0c670e44
BrianOuko <oukobrian001@gmail.com>
2023-03-26 17:02:21 +0300

    Latest Commit

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,25 +1,31 @@
 import * as AWS from 'aws-sdk'
-import { Types } from 'aws-sdk/clients/s3';
+import * as AWSXRay from 'aws-xray-sdk'
 
-export class AttachmentUtils {
+const XAWS = AWSXRay.captureAWS(AWS)
+
+// TODO: Implement the fileStogare logic
+
+const s3BucketName = process.env.ATTACHMENT_S3_BUCKET
+const urlExpiration = 300
+
+export class AttachmentUtils{
     constructor(
-        private readonly s3Client: Types = new AWS.S3({ signatureVersion: 'v4' }),
-        private readonly s3BucketName = process.env.S3_BUCKET_NAME) {
+        private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+        private readonly bucketName = s3BucketName
+    ) {}
+
+    getAttachmentUrl(todoId: string) {
+        return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
     }
 
-
-    async generateUploadUrl(todoId: string): Promise<string> {
-        console.log("Generating URL");
-
-        const url = this.s3Client.getSignedUrl('putObject', {
-            Bucket: this.s3BucketName,
+    getUploadUrl(todoId: string): string {
+        console.log('getUploadUrl called')
+        const url = this.s3.getSignedUrl('putObject', {
+            Bucket: this.bucketName,
             Key: todoId,
-            Expires: 1000,
-        });
-        console.log(url);
+            Expires: urlExpiration
+        })
 
-        return url as string;
+        return url as string
     }
-
-
 }
